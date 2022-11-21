4d98418c936af8fdb6021d74b35764555732ece8
ch-pereira <christofer.pereira.santos2403@gmail.com>
2022-11-21 18:55:15 -0300

    first commit

---
+++ src/main/java/SpringProva/SpringProva/SpringProvaApplication.java
@@ -0,0 +1,13 @@
+package SpringProva.SpringProva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringProva/SpringProva/SpringProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringProva.SpringProva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
